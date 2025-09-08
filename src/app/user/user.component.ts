922f1713f04e76f463faf19c360c93803bc867f1
Artem Gilmanov <artemapochta@gmail.com>
2025-09-08 19:11:38 +0200

    Added Input decorators.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from './dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,19 +8,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  public selectedUser = signal(DUMMY_USERS[randomIndex]);
-  public imagePath = computed(
-    () => `assets/users/${this.selectedUser().avatar}`
-  );
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  // Getter to compute the image path based on the selected user's avatar
-  // get imagePath() {
-  //   return `assets/users/${this.selectedUser.avatar}`;
-  // }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    console.log('User selected:', this.selectedUser);
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
   }
+
+  onSelectUser() {}
 }
