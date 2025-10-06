a78ae4be17887ca72982a1d9140ec57cee94b811
Artem Gilmanov <artemapochta@gmail.com>
2025-10-06 10:57:28 +0200

    Refactor user and task components to enhance functionality and styling
    - Updated user selection logic to manage selected state.
    - Enhanced tasks component to display user-specific tasks with add task functionality.
    - Introduced new task component for task creation.
    - Improved task display with additional details and completion handling.
    - Added CSS styles for better UI presentation.
    - Defined user and task models for type safety.

---
+++ src/app/tasks/task/task.model.ts
@@ -0,0 +1,7 @@
+export interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  date: string;
+  description: string;
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,53 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name?: string | undefined;
+  @Input({ required: true }) userId!: string | undefined;
+  @Input({ required: true }) name!: string | undefined;
+
+  isAddingTask = false;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Task 1',
+      date: '2023-01-01',
+      description: 'Description for Task 1',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Task 2',
+      date: '2023-01-02',
+      description: 'Description for Task 2',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Task 3',
+      date: '2023-01-03',
+      description: 'Description for Task 3',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onTaskCompleted(taskId: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
 }
