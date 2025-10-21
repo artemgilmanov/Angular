8b7ccea5f2d4e475a74c7d4b7758731dac8bc957
Artem Gilmanov <artemapochta@gmail.com>
2025-10-21 09:19:25 +0200

    Refactor application structure by creating AppModule and UserInputModule, removing standalone components.

--- 02-investment-calculator-app/src/app/app.component.ts
+++ 02-investment-calculator-app/src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
 import type { InvestmentResult } from './investment-input.model';
-import { InvestmentResultsComponent } from './investment-result/investment-results.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
   resultsData = signal<InvestmentResult[] | undefined>(undefined);
